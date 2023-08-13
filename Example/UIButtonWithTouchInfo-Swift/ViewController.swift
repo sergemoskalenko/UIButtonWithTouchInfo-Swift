cbff713af1eb104b9083e013859eb34ded7c610d
Serge Moskalenko <sergiy.v.moskalenko@gmail.com>
2023-08-13 18:14:47 +0300

    Update ViewController.swift

--- Example/UIButtonWithTouchInfo-Swift/ViewController.swift
+++ Example/UIButtonWithTouchInfo-Swift/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by sergemoskalenko on 08/02/2017.
 //  Copyright (c) 2017 sergemoskalenko. All rights reserved.
-//  skype:camopu-ympo, http://camopu.rhorse.ru/ios
+//  skype:camopu-ympo
 //
 
 import UIKit
